372f84127b71e7899f9160a564ec46bf5512ad1a
Nicolás Hatcher <nicoliere@gmail.com>
2019-03-02 13:53:01 +0100

    fixes

--- controller.js
+++ controller.js
@@ -3,7 +3,7 @@ console.log('controller js loaded');
 $(function() {
   $('body').html('No GUI for now, controller JS running...')
 
-  var socket = io.connect('localhost:3000/controller');
+  var socket = io.connect('/controller');
 
   console.log('socket is:', socket);
 });
--- viewer.js
+++ viewer.js
@@ -5,7 +5,7 @@ console.log('viewer js loaded');
 $(function() {
   $('body').html('No GUI for now, viewer JS running...')
 
-  const socket = io.connect('localhost:3000/viewer');
+  const socket = io.connect('/viewer');
 
   console.log('socket is:', socket);
 });
