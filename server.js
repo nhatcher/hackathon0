0a16c8c1c7f4fdf4b5f1bc1e00c425af08b2c455
Nicolás Hatcher <nicoliere@gmail.com>
2019-03-02 12:46:05 +0100

    fix stuff

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.use(express.static(__dirname));
+
+app.get('/viewer', function(req, res){
+  res.sendFile(__dirname +  '/viewer.html');
+});
+
+app.get('/controller', function(req, res){
+  res.sendFile(__dirname + '/controller.html');
 });
 
 io.on('connection', function(socket){
