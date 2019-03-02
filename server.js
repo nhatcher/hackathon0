e07ab36b7b5aca84fa2b42d140db26832179b9a6
Nicolás Hatcher <nicoliere@gmail.com>
2019-03-02 13:48:54 +0100

    FIXES

--- server.js
+++ server.js
@@ -16,13 +16,22 @@ app.get('/controller', function(req, res) {
   res.sendFile(__dirname + '/controller.html');
 });
 
-io.on('connection', function(socket) {
-  console.log('a user connected', socket);
+const viewer_socket = io.of('/viewer');
+const controller_socket = io.of('/controller');
+
+viewer_socket.on('connection', function(socket) {
+  console.log('a user connected', socket.id);
   socket.on('disconnect', function() {
     console.log('user disconnected');
   });
 });
 
+controller_socket.on('connection', function(socket) {
+  console.log('Viewer connected', socket.id);
+  socket.on('disconnect', function() {
+    console.log('Viewer disconnected');
+  });
+});
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
