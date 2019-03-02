da67887b42836c352b398c2604b88fdba4d48998
ditam <ditam.github@gmail.com>
2019-03-02 12:31:47 +0100

    add socket.io client

--- controller.js
+++ controller.js
@@ -2,4 +2,8 @@ console.log('controller js loaded');
 
 $(function() {
   $('body').html('No GUI for now, controller JS running...')
+
+  var socket = io();
+
+  console.log('socket is:', socket);
 });
